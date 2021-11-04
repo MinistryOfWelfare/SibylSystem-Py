cc7c2009f4229bab8b2b7efb719b35054cb9f4b1
Sayan Biswas <sayan@pokurt.me>
2021-11-04 16:15:34 +0530

    New Permissions enum so i dont need to remember crap
    Signed-off-by: Sayan Biswas <sayan@pokurt.me>

--- SibylSystem/types/permission.py
+++ SibylSystem/types/permission.py
@@ -1,9 +1,15 @@
 from dataclasses import dataclass
 from typing import Optional
-
+from enum import Enum
 
 @dataclass
 class PermissionResponse:
     success: Optional[bool] = None
     result: Optional[str] = None
     error: Optional[str] = None
+    
+    
+class Permissions(Enum):
+    USER = 0
+    ENFORCER = 1
+    INSPECTOR = 2
